8fc744bcb218993348d748e90d0af65a3410ab6a
CanvasClick <thecanvasclick@gmail.com>
2025-12-08 01:13:05 +0530

    fix: update asset paths in index.html and configure base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: 'newpehnava', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
